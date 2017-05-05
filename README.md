a96d6d2ae4be0f60e5f5fef0c90630622120487f
Fabio Z Tessitore <fabioztessitore@gmail.com>
2017-05-06 00:02:07 +0200

    node-opencv

--- README.md
+++ README.md
@@ -2,46 +2,54 @@
 
 Autore: Diomede Mazzone con la collaborazione di Fabio Z Tessitore (@FabioZTessitore)
 
-Tag: NodeJS, ExpressJS, Socket.io, Python, client/server
+Tag: NodeJS, ExpressJS, Socket.io, OpenCV, client/server
 
-Versione Stabile: 1.0
+Versione: 0.1.0
 
-Licenza: GPL 3.0 o successive
+Licenza: GPL-3.0
 
 ## Descrizione
 
-Questo software ha scopo didattico, consiste in un server che sfrutta la tecnologia Node per
-restituire ad un client connesso sulla porta 3000 un'immagine acquisita da una webcam
-attravero uno script Python.
-Oogni volta che lo script sovrascrive l'immagine il server rileva l'aggiornamento e la spedisce al client.
+Questo software ha scopo didattico.
+
+Consiste in un server che sfrutta la tecnologia Node e le capacità di OpenCV
+per restituire a più client in broadcast una sequenza di immagini acquisita
+da una webcam.
+
+## Prerequisiti
+
+Assicurati di aver installato le librerie OpenCV
+
+```
+$ sudo apt-get install libopencv-dev
+```
 
 ## Requisiti
-*   Raspberry PI (testato su Raspberry PI 2)
-*	  npm (https://www.npmjs.com/)
-*   nodejs (https://nodejs.org/en/)
-*   expressjs (http://expressjs.com/)
-*   socket.io (http://socket.io/)
-*   Python
+* Raspberry PI (testato su Raspberry PI 2) con camera
+* npm (https://www.npmjs.com/)
+* nodejs (https://nodejs.org/en/)
+* expressjs (http://expressjs.com/)
+* socket.io (http://socket.io/)
+* OpenCV (http://opencv.org/)
+* peterbraden/node-opencv (https://github.com/peterbraden/node-opencv)
 
 ## Istallazione e Avvio
 
 * modifica IP: nel file index.html bisogna modificare l'indirizzo IP del server
 
-* l'applicazione server, scritta utilizzando la tecnologia NodeJs, fa uso dei
-moduli ExpressJs e Socket.io.
-Per l'installazione di tali dipendenze:
+* installazione delle dipendenze (attenzione ai prerequisiti):
+
 ```bash
 $ npm install
 ```
-  
-* per l'esecuzione prima bisogna lanciare lo script python, successivamente il server node:
-```bash 
-$ python camera.py
-```
+
+* esecuzione
+
 ```bash
 $ node app.js
 ```
 oppure (per distribuzioni basate su Debian)
+
 ```bash
 $ nodejs app.js
 ```
@@ -53,4 +61,3 @@ a questo punto il server è in ascolto sulla porta 3000.
 
 
 = Ringraziamenti =
-
--- app.js
+++ app.js
@@ -1,38 +1,60 @@
 var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
-var fs = require('fs');
+var cv = require('opencv');
 
-server.listen(3000);
+var SERVER_PORT = 3000;
 
-// usa express per gestire il routing
-//
-// quando il client richiede (con metodo GET) il
-// percorso '/', invia il file index.html
-app.get('/', function(req, res) {
-  res.sendFile(__dirname+'/index.html');
+var FRAME_DELAY = 100;  // msec
+var JPEG_QUALITY = 70;
+
+// numero di client collegati
+var clients = 0;
+
+try {
+  var camera = new cv.VideoCapture(0);
+} catch (e) {
+  console.log("Couldn't start camera: ", e);
+  process.exit(1);
+}
+
+server.listen(SERVER_PORT);
+console.log('server listening on port ' + SERVER_PORT);
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
 });
 
-// usa socket.io per registrare le azioni da compiere
-// quando si verificano certi eventi.
-//
-// Gestisce l'evento 'connection'
-io.on('connection', function(socket) {
-  // all'avvio di una connessione viene creato un socket!
+io.on('connection', function (socket) {
+  clients++;
+  console.log('stabilita nuova connessione');
+  console.log('client collegati: ', clients);
 
-  console.log('user connected');
-
-  // a connessione avvenuta,
-  // usa il modulo fs per monitorare il file image.jpg
-  fs.watch(__dirname+'/pictures/image.jpg', function(evt, filename) {
-    console.log('rilevata variazione immagine');
-
-    // in caso di variazione del file image.jpg lo reinvia
-    // al client tramite il socket precedentemente creato
-    fs.readFile(__dirname+'/pictures/image.jpg', function(err, buf) {
-      socket.emit('update_image', {image: true, buffer: buf.toString('base64')});
-    });
+  socket.on('disconnect', function () {
+    clients--;
+    console.log('connessione chiusa');
+    console.log('client collegati: ', clients);
   });
 });
 
-console.log('server listening on port 3000');
+
+setInterval(function() {
+  camera.read(function(err, im) {
+    if (err) throw err;
+
+    if (clients > 0) {
+      //im.convertGrayscale();
+
+      im.resize(320, 240);
+
+      var s = im.toBuffer({
+        ext: ".jpg",
+        jpegQuality: JPEG_QUALITY
+      }).toString('base64');
+      console.log(s.length);
+
+      // broadcast the frame
+      io.emit('frame', { buffer: s });
+    }
+  });
+}, FRAME_DELAY);
