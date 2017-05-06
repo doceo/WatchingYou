dc949987b6465c9d0537db6bfaa3eca8cf606b5b
Fabio Z Tessitore <fabioztessitore@gmail.com>
2017-05-06 11:47:49 +0200

    legge camera solo in presenza di client

--- app.js
+++ app.js
@@ -5,11 +5,13 @@ var cv = require('opencv');
 
 var SERVER_PORT = 3000;
 
-var FRAME_DELAY = 100;  // msec
+var FRAME_DELAY = 40;  // msec
 var JPEG_QUALITY = 70;
 
 // numero di client collegati
 var clients = 0;
+
+var tID;
 
 try {
   var camera = new cv.VideoCapture(0);
@@ -29,32 +31,37 @@ io.on('connection', function (socket) {
   clients++;
   console.log('stabilita nuova connessione');
   console.log('client collegati: ', clients);
+  if (clients == 1) startCamera();
 
   socket.on('disconnect', function () {
     clients--;
+    if (clients <= 0) stopCamera();
     console.log('connessione chiusa');
     console.log('client collegati: ', clients);
   });
 });
 
+function startCamera() {
+  tID = setInterval(function() {
+    camera.read(function(err, im) {
+      if (err) throw err;
 
-setInterval(function() {
-  camera.read(function(err, im) {
-    if (err) throw err;
-
-    if (clients > 0) {
       //im.convertGrayscale();
 
       im.resize(320, 240);
 
       var s = im.toBuffer({
         ext: ".jpg",
-        jpegQuality: JPEG_QUALITY
+        jpegQuality: JPEG_QUALITY,
       }).toString('base64');
-      console.log(s.length);
+      //console.log(s.length);
 
       // broadcast the frame
       io.emit('frame', { buffer: s });
-    }
-  });
-}, FRAME_DELAY);
+    });
+  }, FRAME_DELAY);
+}
+
+function stopCamera() {
+  clearInterval(tID);
+}
